1595cd0d6c2b7614b48e57b7e3a22a5711f6ac84
Roberto Boscolo <beto.boscolo@gmail.com>
2021-05-18 21:16:40 -0300

    First commit!

---
+++ src/main/java/com/boscolo/deliveryapi/controller/ClienteController.java
@@ -0,0 +1,21 @@
+package com.boscolo.deliveryapi.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boscolo.deliveryapi.domain.model.Cliente;
+
+@RestController
+public class ClienteController {
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		Cliente cliente1 = new Cliente();
+		cliente1.setNome("teste");
+		return Arrays.asList(cliente1);
+	}
+
+}
---
+++ src/main/java/com/boscolo/deliveryapi/domain/model/Cliente.java
@@ -0,0 +1,14 @@
+package com.boscolo.deliveryapi.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
+
+}
