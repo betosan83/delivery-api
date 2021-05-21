ad16ab3c0b7d1169c107fc892c6db6454ee8f7f0
Roberto Boscolo <beto.boscolo@gmail.com>
2021-05-21 06:41:32 -0300

    First entities and endpoints for Clients

--- src/main/java/com/boscolo/deliveryapi/controller/ClienteController.java
+++ src/main/java/com/boscolo/deliveryapi/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.boscolo.deliveryapi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,15 @@ import com.boscolo.deliveryapi.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		Cliente cliente1 = new Cliente();
 		cliente1.setNome("teste");
-		return Arrays.asList(cliente1);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
--- src/main/java/com/boscolo/deliveryapi/domain/model/Cliente.java
+++ src/main/java/com/boscolo/deliveryapi/domain/model/Cliente.java
@@ -1,14 +1,31 @@
 package com.boscolo.deliveryapi.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "")
 public class Cliente {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 
 }
