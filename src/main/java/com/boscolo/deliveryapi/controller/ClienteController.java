e7f7d72ace2e50c79b79a3dc8f339f81c4658154
Roberto Boscolo <beto.boscolo@gmail.com>
2021-05-23 22:51:17 -0300

    Crud finished.

--- src/main/java/com/boscolo/deliveryapi/controller/ClienteController.java
+++ src/main/java/com/boscolo/deliveryapi/controller/ClienteController.java
@@ -1,12 +1,17 @@
 package com.boscolo.deliveryapi.controller;
 
 import java.util.List;
-import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boscolo.deliveryapi.domain.model.Cliente;
@@ -34,6 +39,36 @@ public class ClienteController {
 //				.map(cliente -> ResponseEntity.ok(cliente))
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionarCliente(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("/{clienteId}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, 
+			@RequestBody Cliente cliente) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(clienteId);
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{clienteId}")
+	public ResponseEntity<Void> remover(@PathVariable Long clienteId) {
+		if (!clienteRepository.existsById(clienteId)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(clienteId);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
